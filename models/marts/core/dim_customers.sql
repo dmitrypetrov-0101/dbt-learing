e33b91ba4ab3438da06854ba6593f5bde0eec0e8
dmitry.petrov@zoho.com <dmitry.petrov@zoho.com>
2021-09-22 20:36:44 +0000

    adds lifetime_value to dim_customers

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -15,11 +15,14 @@ customer_orders as (
     select
         customer_id,
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        min(orders.order_date) as first_order_date,
+        max(orders.order_date) as most_recent_order_date,
+        count(orders.order_id) as number_of_orders,
+        sum(fct_orders.amount) as lifetime_value
 
     from orders
+
+    left join {{ ref('fct_orders') }} as fct_orders using (customer_id)
 
     group by 1
 
@@ -33,7 +36,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_orders.lifetime_value, 0) as lifetime_value
 
     from customers
 
